596d0017a4989bf50e296843f0ae3ff30e2f1fd3
Choquet Nicolas <nicolachoquet06250@gmail.com>
2025-12-05 18:34:28 +0100

    fix bugs

--- example/assets/js/helpers.mjs
+++ example/assets/js/helpers.mjs
@@ -1,2 +1,2 @@
 /** @param {string} str */
-export const toUcFirst = str =>`${str.substring(0, 1).toUpperCase()}${str.substring(1)}`;
+export const toUcFirst = str =>`${str?.substring(0, 1).toUpperCase()}${str?.substring(1)}`;
